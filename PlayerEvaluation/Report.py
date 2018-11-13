f0a818df2f74337df42ce3b97618390f9219560f
William N. Roney <wroney@roneymis.com>
2018-11-13 15:53:01 -0500

    Added multiple coach support

--- PlayerEvaluation/Report.py
+++ PlayerEvaluation/Report.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
@@ -8,18 +9,19 @@ class Report:
 
     def __init__(self):
         self.DataFrame = None
+        self.coach_cnt = 0
         return 
 
     def load(self, ifile):
         if (self.DEBUG): print "\033[4;34mLoading Dataset from : ", ifile, "\033[0m"
         self.DataFrame = pd.read_excel(ifile)
-        #print type(self.DataFrame)
-        #print self.DataFrame
+        if (self.DEBUG): print "\033[4;34m\tCoach Count: ", self.coach_cnt, "\033[0m"
         return
         
     def saveResult(self, ifile, ofname):
         self.load(ifile)
         if (self.DEBUG): print "\033[4;34mSaving Results to: ", ofname, "\033[0m"
+
         rptfile = open(ofname, 'w+')
         rptfile.write("<html><title>Player Evaluations</title><body>")
         rptfile.write("<h1>Team Summary</h1>")
@@ -34,8 +36,13 @@ class Report:
         rptfile.write("</td><td width=\"50%\">")
         rptfile.write(self.outputPsychological())
         rptfile.write("</td></tr></table>")
-        rptfile.write("<p style=\"page-break-after: always;\">")
+        rptfile.write("</body></html>")
+        rptfile.close()
+        
         for player in self.DataFrame.PlayerName.unique():
+            rptfile = open(player+".html", 'w+')
+            if (self.DEBUG): print "\033[4;34mSaving Results to: ", (player+".html"), "\033[0m"
+            rptfile.write("<html><title>Player Evaluations</title><body>")
             rptfile.write(self.outputHeader(player))
             rptfile.write("<p><table border=\"0\">")
             rptfile.write("<tr><td width=\"50%\">")
@@ -48,17 +55,17 @@ class Report:
             rptfile.write(self.outputPsychological(player))
             rptfile.write("</td></tr></table><hr>")
             rptfile.write(self.outputCommentary(player))
-            rptfile.write("<p style=\"page-break-after: always;\">")
+            rptfile.write("</body></html>")
+            rptfile.close()
         
-        rptfile.write("</body></html>")
-        rptfile.close()
         return
         
     def outputHeader(self, player=None):
+        if (self.DEBUG): print "\033[4;34m\tOutputting Header\033[0m"
         fig, bp = plt.subplots(nrows=1, ncols=1)
         fig.set_size_inches(2.5,2.5)
-        years = list(self.DataFrame[(self.DataFrame.Rater == "Coach")].BirthYear)
-        mine = list(self.DataFrame[(self.DataFrame.Rater == "Coach") & (self.DataFrame.PlayerName == player)].BirthYear)
+        years = list(self.DataFrame[(self.DataFrame.Rater.str.match("Coach*"))].BirthYear)
+        mine = list(self.DataFrame[(self.DataFrame.Rater.str.match("Coach*")) & (self.DataFrame.PlayerName == player)].BirthYear)
         period = self.DataFrame.Period[0]
         bp.set_ylim(top=min(years)-2, bottom=max(years)+2)
         bp.violinplot(years, showextrema=False)
@@ -85,6 +92,7 @@ class Report:
         return buffer
 
     def genGraph(self, name, title, cats):
+        if (self.DEBUG): print "\033[4;34m\t\tgenGraph\033[0m"
         buffer = ""
         fig, bp = plt.subplots(nrows=1, ncols=1)
         fig.set_size_inches(4.5,3.5)
@@ -101,13 +109,13 @@ class Report:
         myratings = []
         coachratings = []
         for cat in cats:
-            samps = list(self.DataFrame[self.DataFrame.Rater == "Coach"][cat])
+            samps = list(self.DataFrame[self.DataFrame.Rater.str.match("Coach*")][cat]) #whole team
             me = list(self.DataFrame[(self.DataFrame.Rater == "Player") & (self.DataFrame.PlayerName == name)][cat])
-            coach = list(self.DataFrame[(self.DataFrame.Rater == "Coach") & (self.DataFrame.PlayerName == name)][cat])
+            coaches = list(self.DataFrame[(self.DataFrame.Rater.str.match("Coach*")) & (self.DataFrame.PlayerName == name)][cat])
             plotdata.append(samps)
             if len(me) > 0: myratings.append(me)
             else:  myratings.append([0])
-            if len(coach) > 0: coachratings.append(coach)
+            if len(coaches) > 0: coachratings.append(np.mean(coaches))
             else:  coachratings.append([0])
         bp.violinplot(plotdata, showextrema=False)
         plt.ylim(0.5, len(ratings)+0.5)
@@ -117,7 +125,7 @@ class Report:
 
         if name is not None:
             bp.scatter(xt, myratings, label="Player", color='r', marker='>')
-            bp.scatter(xt, coachratings, label="Coach", color='b', marker='<')
+            bp.scatter(xt, coachratings, label="Coaches", color='b', marker='<')
             bp.legend(fontsize='x-small')
             
 
@@ -132,6 +140,7 @@ class Report:
         return buffer
         
     def outputTechnical(self, name=None):
+        if (self.DEBUG): print "\033[4;34m\tOutputting Technical\033[0m"
         cats = ["Dribbling",
                 "LongPass",
                 "ShortPass",
@@ -143,6 +152,7 @@ class Report:
         return self.genGraph(name, "Technical", cats)
         
     def outputTactical(self, name=None):
+        if (self.DEBUG): print "\033[4;34m\tOutputting Tactical\033[0m"
         cats = ["DecisionMaking",
                 "SpeedOfPlay",
                 "FieldVision",
@@ -154,6 +164,7 @@ class Report:
         return self.genGraph(name, "Tactical", cats)
         
     def outputPhysical(self, name=None):
+        if (self.DEBUG): print "\033[4;34m\tOutputting Physical\033[0m"
         cats = ["Speed",
                 "Agility",
                 "Quickness",
@@ -165,6 +176,7 @@ class Report:
         return self.genGraph(name, "Physical", cats)
         
     def outputPsychological(self, name=None):
+        if (self.DEBUG): print "\033[4;34m\tOutputting Psychological\033[0m"
         cats = ["Composure",
                 "Commitment",
                 "Leadership",
@@ -176,6 +188,7 @@ class Report:
         return self.genGraph(name, "Psychological", cats)
         
     def outputCommentary(self, name):
+        if (self.DEBUG): print "\033[4;34m\tOutputting Commentary\033[0m"
         coachcomment = ""
         playercomment = ""
         comments = self.DataFrame.set_index("PlayerName", drop = False).loc[name, ["Rater", "Comments"]].values
@@ -191,7 +204,7 @@ class Report:
                 elif com[0] == "Player":
                     playercomment = com[1]
         buffer = "<table>"
-        buffer = buffer + "<tr><td>Coach Comments:</td><td>" + coachcomment + "</td><tr>"
-        buffer = buffer + "<tr><td>Player Comments:</td><td>" + playercomment + "</td><tr>"
+        buffer = buffer + "<tr><td>Coach Comments:</td><td>" + coachcomment + "</td></tr>"
+        buffer = buffer + "<tr><td>Player Comments:</td><td>" + playercomment + "</td></tr>"
         buffer = buffer + "</table>"
         return buffer
